1e7fec224ee69064ad21f16a0d007daeb17be1be
CzarnecD <dawid.czarnecki@qiagen.com>
2022-05-24 11:42:51 +0200

    added logging, still not too comprehensive

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -1,14 +1,12 @@
 package hellocucumber;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
 import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
-//import io.cucumber.junit.*;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
